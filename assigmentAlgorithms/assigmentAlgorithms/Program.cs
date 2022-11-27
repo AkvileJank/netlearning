503aeee18aa855dc4b43d3e029ff34078828ad3a
Akvile Jankauskaite <akvile.jankauskait@gmail.com>
2022-11-27 22:40:26 +0200

    enhanced calculator

--- assigmentAlgorithms/assigmentAlgorithms/Program.cs
+++ assigmentAlgorithms/assigmentAlgorithms/Program.cs
@@ -2,44 +2,66 @@
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Enter first number:");
-        double num1 = Double.Parse(Console.ReadLine());
-        Console.WriteLine("Enter second number:");
-        double num2 = Double.Parse(Console.ReadLine());
-        Console.WriteLine("Enter math operation symbol (* - multiplication, / - division, % - modulo, + - addition, - - subtraction)");
-        string symbol = Console.ReadLine();
+    
+        double num1 = 0;
+        double num2 = 0;
+        bool validInputs = true;
 
-        double result = 0;
+            try
+            {
+                Console.WriteLine("Enter first number:");
+                num1 = Double.Parse(Console.ReadLine());
+                Console.WriteLine("Enter second number:");
+                num2 = Double.Parse(Console.ReadLine());
+            }
+            catch
+            {
+                Console.WriteLine("Input is not valid");
+                validInputs = false;
+            }
 
-        if (symbol == "*")
-            result = num1 * num2;
-        else if (symbol == "/")
+        if (validInputs)
         {
-            if (num2 == 0)
-                Console.WriteLine("Operation is not valid");
+            Console.WriteLine("Enter math operation symbol (* - multiplication, / - division, % - modulo, + - addition, - - subtraction)");
+            string symbol = Console.ReadLine();
+
+            double result = 0;
+
+            if (symbol == "*")
+                result = num1 * num2;
+            else if (symbol == "/")
+            {
+                if (num2 == 0)
+                    Console.WriteLine("Operation is not valid");
+                else
+                    result = num1 / num2;
+            }
+            else if (symbol == "%")
+            {
+                if (num2 == 0)
+                    Console.WriteLine("Operation is not valid");
+                else
+                    result = num1 % num2;
+            }
+            else if (symbol == "+")
+                result = num1 + num2;
+            else if (symbol == "-")
+                result = num1 - num2;
             else
-                result = num1 / num2;
+                Console.WriteLine("Operation symbol is not valid");
+
+            Console.WriteLine(num1 + symbol + num2 + "=" + result);
+
+
+
+
+
+
+
+
+
+
         }
-        else if (symbol == "%")
-        {
-            if (num2 == 0)
-                Console.WriteLine("Operation is not valid");
-            else
-                result = num1 % num2;
-        }
-        else if (symbol == "+")
-            result = num1 + num2;
-        else if (symbol == "-")
-            result = num1 - num2;
-        else
-            Console.WriteLine("Operation symbol is not valid");
-
-        Console.WriteLine(num1 + symbol + num2 + "=" + result);
-
-
-
-
-
 
     }
 }
