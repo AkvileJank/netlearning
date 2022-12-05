45d5b61bb809401f5dd88ed1eab9df86351086b3
Akvile Jankauskaite <akvile.jankauskait@gmail.com>
2022-12-05 21:39:32 +0200

    Edit2 phone number

--- Practical7/Practical7/Program.cs
+++ Practical7/Practical7/Program.cs
@@ -5,21 +5,16 @@ class PhoneNumberTask
     public static void Main(string[] args)
     {
 
-        Console.WriteLine("Enter your phone number:");
-        string phoneNum = Console.ReadLine();
-
+       Console.WriteLine("Enter your phone number:");
+       string phoneNum = Console.ReadLine();
        bool match = Regex.Match(phoneNum, @"(\+370){0,1} {0,1}[0-9]{8}$").Success;
         if (match)
         {
-            match = Regex.Match(phoneNum, @"(\+370){0,1} {0,1}[0-9]{8}$").Success;
             Console.WriteLine("Phone number provided is correct");
         }
-
         else
         {
             Console.WriteLine("Phone number provided is not correct");
         }
-        
-
     }
 }
