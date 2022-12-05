927d9108d51733625c87e6dcc9d4a1ebc3fcbf79
Akvile Jankauskaite <akvile.jankauskait@gmail.com>
2022-12-05 21:22:31 +0200

    Phone number task

---
+++ Practical7/Practical7/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Text.RegularExpressions;
+
+class PhoneNumberTask
+{
+    public static void Main(string[] args)
+    {
+
+        Console.WriteLine("Enter your phone number:");
+        string phoneNum = Console.ReadLine();
+        //var match = Regex.Match(phoneNum, ((@"\+370[0-9]{8}$") || (@"\+370 [0-9]{8}$") || (@"[0-9]{8}$"));
+
+        bool match = true;
+
+        if(match)
+        {
+            match = Regex.Match(phoneNum, @"\+370[0-9]{8}$").Success || Regex.Match(phoneNum, @"\+370 [0-9]{8}$").Success || Regex.Match(phoneNum, @"\[0-9]{8}$").Success;
+            Console.WriteLine("Phone number provided is correct");
+        }
+
+        else
+        {
+            Console.WriteLine("Phone number provided not correct");
+        }
+        
+
+
+
+
+
+
+    }
+}
